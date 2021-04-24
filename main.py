8bf5d7d543fa4e817054892476a5127f3065b88a
emilg <emilgabrielli@gmail.com>
2021-04-24 13:41:13 +0200

    Added the main.py script which for now only consist of the worker class

---
+++ main.py
@@ -0,0 +1,15 @@
+import random
+
+class Worker:
+    counter = 0
+    def __init__(self, cores):
+        self.cores = cores
+        self.id = self.give_id()
+
+    def __str__(self):
+        return f"{self.id}"
+
+    def give_id(self):
+        Worker.counter += 1
+        id = "W"+str(Worker.counter).zfill(2)+f"-C{self.cores}"
+        return id
