8eac74227b9cd52c7a2bac7ccf440ce865370592
emilg <emilgabrielli@gmail.com>
2021-04-26 01:16:20 +0200

    Added more unit tests in I/O, successfully imported unit tests into the main scripts and added comments

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
-import time
+# Libraries #
+import time # used for measuring completion times
+import unittest # used for running unit tests
+
+# processing external unit tests #
+# 1: Imports the unit tests classes from external scripts
+from unittests import io, calculations, strings
+# 2: Creates test suites from every class
+io_suite = unittest.TestLoader().loadTestsFromModule(io)
+calculations_suite = unittest.TestLoader().loadTestsFromModule(calculations)
+strings_suite = unittest.TestLoader().loadTestsFromModule(strings)
+####
 
 class Worker:
     counter = 0
@@ -29,9 +40,17 @@ class TestInfrastructure:
     def run_unittest(self, test):
         pass
 
+# Flag 1: Getting start time for measuring completion times
 start_time = time.monotonic_ns()
 
+# Runs the unit test suites with verbosity 0 (quiet)
+unittest.TextTestRunner(verbosity=0).run(io_suite)
+unittest.TextTestRunner(verbosity=0).run(calculations_suite)
+unittest.TextTestRunner(verbosity=0).run(strings_suite)
 
-
+# Flag 2: Getting the stop time for measuring completion times
 end_time = time.monotonic_ns()
+# Calculates the completion time in seconds instead of nanoseconds
 completion_time_s = (end_time-start_time)/10**9
+# Outputs the completion time
+print("Completion time: ",completion_time_s)
--- unittests/io.py
+++ unittests/io.py
@@ -12,5 +12,18 @@ class TestIO(unittest.TestCase):
                 alc_total += float(split_row[1])
         self.assertEqual(alc_total/wine_amount,13.000617977528083)
 
+    def test_pokerhands(self):
+        three_of_a_kind = 0
+        royals = 0
+        with open("testfiles/poker-hand-training-true.csv") as hands:
+            for row in hands.readlines():
+                split_row = row.split(",")
+                if split_row[-1][0] == "9":
+                    royals += 1
+                elif split_row[-1][0] == "3":
+                    three_of_a_kind += 1
+        self.assertEqual(royals,5)
+        self.assertEqual(three_of_a_kind,513)
+
 if __name__ == '__main__':
     unittest.main()
