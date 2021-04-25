eb37282532a1fe193ca2e5fc830228ce7580dc81
emilg <emilgabrielli@gmail.com>
2021-04-25 14:42:24 +0200

    Added unit test on wine

--- unittests/io.py
+++ unittests/io.py
@@ -2,7 +2,15 @@ import unittest
 
 class TestIO(unittest.TestCase):
 
-
+    def test_wine(self):
+        wine_amount = 0
+        alc_total = 0
+        with open("testfiles/wine.csv","r") as wine:
+            for row in wine.readlines()[1:]:
+                wine_amount += 1
+                split_row = row.split(",")
+                alc_total += float(split_row[1])
+        self.assertEqual(alc_total/wine_amount,13.000617977528083)
 
 if __name__ == '__main__':
     unittest.main()
