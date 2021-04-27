e683d42f99ef4ebac3fcdc5e8b5255e98b5341d7
emilg <emilgabrielli@gmail.com>
2021-04-27 23:31:46 +0200

    Created the complete classes for Worker and Test and started working on the preface of the simulation that will take the input files and create workers and tests

--- main.py
+++ main.py
@@ -1,29 +1,48 @@
 # Classes
 class Worker:
-    counter = 0
-    def __init__(self):
-        self.performance_rating = 0
-        self.id = self.give_id()
-
+    def __init__(self, WorkerID, StartTimeAvailable, PerformanceRating, AvailabilityTime):
+        self.WorkerID = str(WorkerID)
+        self.StartTimeAvailable = float(StartTimeAvailable)
+        self.PerformanceRating = float(PerformanceRating)
+        self.AvailabilityTime = float(AvailabilityTime)
     def __str__(self):
-        return f"{self.id}"
+        return f"{self.WorkerID}: \n\tStarted working: {self.StartTimeAvailable}\
+        \n\tPerformance: {self.PerformanceRating*100}%\
+        \n\tRemaining availability: {self.AvailabilityTime}"
 
-    def give_id(self):
-        Worker.counter += 1
-        id = "W"+str(Worker.counter).zfill(2)+f"-C{self.cores}"
-        return id
-
-    def run_unittest(self, test):
-        pass
-
-class TestInfrastructure:
-
-    def __init__(self):
-        self.performance_rating = 0
-        self.id = "TestingInfrastructure1"
-
+class Test:
+    def __init__(self, TestID, ScheduledTime, TestTime):
+        self.TestID = str(TestID)
+        self.ScheduledTime = float(ScheduledTime)
+        self.TestTime = float(TestTime)
     def __str__(self):
-        return f"{self.id}"
+        return f"{self.TestID}: \n\tScheduled for testing at: {self.ScheduledTime}\
+        \n\tTime it takes for testing: {self.TestTime}"
 
-    def run_unittest(self, test):
-        pass
+#### PRE-FACE ####
+# Start Time
+time = 0.00
+
+# Reference the workerlist file to simulate worker scenario
+worker_scenario = "workerlists/1initial_increase_in_worktimes.csv"
+
+# Reference the tasklist file to simulate test scenario
+test_scenario = "tasklists/steady_flow.csv"
+
+# NOTE:
+# The following dictionaries are marked as "queues".
+# These dictionaries will contain ALL tests and ALL workers in the scenario.
+# The dictionaries will be checked by our simulation to know when a worker is available or when a test is submitted.
+# Creates a dictionary that will later be used to store the worker objects as values where the key is the workerID
+worker_dictionary_queue = {}
+with open(worker_scenario,"r") as csv_file:
+    for row in csv_file.readlines()[1:]:
+        split_row = row.split(",")
+        worker_dictionary_queue[split_row[0]] = Worker(split_row[0],split_row[1],split_row[2],split_row[3])
+
+test_dictionary_queue = {}
+with open(test_scenario,"r") as csv_file:
+    for row in csv_file.readlines()[1:]:
+        split_row = row.split(",")
+        test_dictionary_queue[split_row[0]] = Test(split_row[0],split_row[1],split_row[2])
+##################
